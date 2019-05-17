c49a346baa4a97f457aa11900db8cab4e814d173
haiwy <969285375@qq.com>
2019-05-17 18:50:01 +0800

    部分提交测试

---
+++ backend/controllers/MyTestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use yii\web\NotFoundHttpException;
+
+
+
+/**
+ * AuthItemController implements the CRUD actions for AuthItem model.
+ */
+class MyTestController extends AuthController
+{
+	public function actionIndex(){
+		echo 'this method finished';
+	}
+
+	public function actionCreate(){
+		echo 'To be perfected';
+	}
+}
